33124538069aed18be51c453739908eaa9557203
Kate Pronina <kateadm@narod.ru>
2019-11-11 19:05:41 +0300

    Домашнее задание

---
+++ variables/main.js
@@ -0,0 +1,35 @@
+let algebra, geography, physics;
+
+algebra = 5;
+geography = 4;
+physics = 5;
+
+let middle = (algebra + geography + physics) / 3;
+
+console.log(middle);
+
+document.write(middle);
+
+
+
+let myName = "Екатерина";
+
+message = `\n Привет, мир! Меня зовут ${myName}.`;
+
+console.log(message);
+
+document.write(message);
+
+
+
+let x, y, z;
+
+x = 2;
+y = 22;
+z = 0;
+
+result = (x*y) + (z*5) + x - 1;
+
+console.log(result);
+
+document.write(result);
